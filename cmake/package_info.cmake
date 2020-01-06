da67d896d5d64f4ec3234e7e42be9b0d6eee54ec
Angelo Theodorou <encelo@gmail.com>
2020-01-06 10:36:45 +0100

    Update copyright dates to 2020
    - Update NDK and Linux CMake versions in Azure Pipeline configuration

--- cmake/package_info.cmake
+++ cmake/package_info.cmake
@@ -1,7 +1,7 @@
 set(PACKAGE_NAME "ncTemplate")
 set(PACKAGE_EXE_NAME "nctemplate")
 set(PACKAGE_VENDOR "Angelo Theodorou")
-set(PACKAGE_COPYRIGHT "Copyright ©2019 ${PACKAGE_VENDOR}")
+set(PACKAGE_COPYRIGHT "Copyright ©2019-2020 ${PACKAGE_VENDOR}")
 set(PACKAGE_DESCRIPTION "A template project for applications and games made with the nCine")
 set(PACKAGE_HOMEPAGE "https://ncine.github.io")
 set(PACKAGE_REVERSE_DNS "io.github.ncine.nctemplate")
