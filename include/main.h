141395d8fec6f7c0fdb2e86b1c887c0f65898a48
Antonio Caggiano <info@antoniocaggiano.eu>
2020-12-12 12:41:56 +0100

    misc: Change clang format
    My game, my formatting rules.

--- include/main.h
+++ include/main.h
@@ -4,8 +4,8 @@
 #include <ncine/IAppEventHandler.h>
 #include <ncine/IInputEventHandler.h>
 
-namespace ncine {
-
+namespace ncine
+{
 class AppConfiguration;
 
 }
@@ -13,14 +13,12 @@ class AppConfiguration;
 namespace nc = ncine;
 
 /// My nCine event handler
-class MyEventHandler :
-    public nc::IAppEventHandler,
-    public nc::IInputEventHandler
+class MyEventHandler : public nc::IAppEventHandler, public nc::IInputEventHandler
 {
-  public:
-	void onPreInit(nc::AppConfiguration &config) override;
+public:
+    void onPreInit(nc::AppConfiguration &config) override;
 
-	void onKeyReleased(const nc::KeyboardEvent &event) override;
+    void onKeyReleased(const nc::KeyboardEvent &event) override;
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,33 @@
 #include "main.h"
 
-#include <ncine/Application.h>
 #include <ncine/AppConfiguration.h>
+#include <ncine/Application.h>
 
 nctl::UniquePtr<nc::IAppEventHandler> createAppEventHandler()
 {
-	return nctl::makeUnique<MyEventHandler>();
+    return nctl::makeUnique<MyEventHandler>();
 }
 
 void MyEventHandler::onPreInit(nc::AppConfiguration &config)
 {
 #if defined(__ANDROID__)
-	config.dataPath() = "asset::";
+    config.dataPath() = "asset::";
 #elif defined(__EMSCRIPTEN__)
-	config.dataPath() = "/";
+    config.dataPath() = "/";
 #else
-	#ifdef PACKAGE_DEFAULT_DATA_DIR
-	config.dataPath() = PACKAGE_DEFAULT_DATA_DIR;
-	#else
-	config.dataPath() = "data/";
-	#endif
+#ifdef PACKAGE_DEFAULT_DATA_DIR
+    config.dataPath() = PACKAGE_DEFAULT_DATA_DIR;
+#else
+    config.dataPath() = "data/";
+#endif
 #endif
 
-	config.windowTitle = "ncTemplate";
-	config.windowIconFilename = "icon48.png";
+    config.windowTitle = "ncTemplate";
+    config.windowIconFilename = "icon48.png";
 }
 
 void MyEventHandler::onKeyReleased(const nc::KeyboardEvent &event)
 {
-	if (event.sym == nc::KeySym::ESCAPE)
-		nc::theApplication().quit();
+    if (event.sym == nc::KeySym::ESCAPE)
+        nc::theApplication().quit();
 }
