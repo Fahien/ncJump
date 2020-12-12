3ea86754287b008def243a069d7a1a8ca24e7895
Antonio Caggiano <info@antoniocaggiano.eu>
2020-12-12 22:57:48 +0100

    game: Entity state
    Introduce a state enumeration for entities, with idle as default.

--- src/entity.cpp
+++ src/entity.cpp
@@ -7,16 +7,26 @@ namespace nc = ncine;
 
 namespace jmp
 {
+const char* to_string(Entity::State state)
+{
+    switch (state) {
+    case Entity::State::IDLE:
+        return "IDLE";
+    default:
+        FATAL_ASSERT_MSG(false, "Failed to get string for character state");
+    }
+}
+
 #define PATH(path) nc::fs::joinPath(nc::fs::dataPath(), path).data()
 
 Entity::Entity()
     : texture {PATH("img/hero/herochar_idle_anim_strip_4.png")}
-    , sprite {&nc::theApplication().rootNode(), &texture}
+    , idle {&nc::theApplication().rootNode(), &texture}
 {
     texture.setMagFiltering(nc::Texture::Filtering::NEAREST);
 
-    sprite.x += 100;
-    sprite.y += 50;
+    idle.x += 100;
+    idle.y += 50;
 
     auto anim = nc::RectAnimation(
         0.125, nc::RectAnimation::LoopMode::ENABLED, nc::RectAnimation::RewindMode::FROM_START);
@@ -27,8 +37,8 @@ Entity::Entity()
         anim.addRect(nc::Recti(i * sprite_size.x, 0.0f, sprite_size.x, sprite_size.y));
     }
 
-    sprite.addAnimation(nctl::makeUnique<nc::RectAnimation>(anim));
-    sprite.setPaused(false);
+    idle.addAnimation(nctl::makeUnique<nc::RectAnimation>(anim));
+    idle.setPaused(false);
 }
 
 } // namespace jmp
