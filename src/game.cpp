679cf6fb47f34210a030cb66ed0c444253bd7725
Antonio Caggiano <info@antoniocaggiano.eu>
2020-12-17 00:54:48 +0100

    game: Physics and collisions
    Use Box2D for physics and collisions, and test it with a simple scene.

--- src/game.cpp
+++ src/game.cpp
@@ -13,6 +13,7 @@ Game::Game(Config& config)
     , resource {PATH("img/tile/tileset.png")}
     , tileset {resource, config.size.tile}
     , tilemap {scene, tileset}
+    , physics {entity.node.x, entity.node.y}
 {
     resource.setMagFiltering(nc::Texture::Filtering::NEAREST);
     root.setScale(config.scale.global);
@@ -24,6 +25,13 @@ void Game::update(const f32 dt)
     // Update game state
     entity.move(input.move);
     editor.update(*this);
+    physics.update(dt);
+
+    // @todo Move this somewhere else?
+    // Update entity from box
+    auto& pos = physics.hero_body->GetPosition();
+    entity.node.x = config.size.tile * pos.x + config.size.tile / 2.0f;
+    entity.node.y = config.size.tile * pos.y + config.size.tile / 2.0f;
 }
 
 } // namespace jmp
