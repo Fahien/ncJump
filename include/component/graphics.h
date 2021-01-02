fca31498315642ed11649dbb99552b4a499d23d4
Antonio Caggiano <info@antoniocaggiano.eu>
2021-01-02 18:33:19 +0100

    cmp: Character graphics component
    Refactor graphics related stuff moving it from Entity into its own
    component. Here we introduce a class hierarchy where the most basic
    graphics component has a single static image. At the same time we
    introduce a Character graphics component with resources for a character
    state such as idle, movement, jump, and fall.

--- include/component/graphics.h
+++ include/component/graphics.h
@@ -1,9 +1,46 @@
 #pragma once
 
+#include <ncine/AnimatedSprite.h>
+#include <ncine/Texture.h>
+
+#include "types.h"
+
 namespace jmp
 {
+struct Input;
+class TransformComponent;
+
 class GraphicsComponent {
 public:
+    virtual void update(const Input& input) = 0;
+};
+
+// Graphics component with a single static image which never changes
+class SingleGraphicsComponent : public GraphicsComponent {
+public:
+    void update(const Input& input) override;
+
+    nc::Texture idle_texture;
+    nc::Sprite idle;
+};
+
+class CharacterGraphicsComponent : public GraphicsComponent {
+public:
+    CharacterGraphicsComponent(TransformComponent& t);
+
+    void update(const Input& input) override;
+
+    nc::Texture idle_texture;
+    nc::AnimatedSprite idle;
+
+    nc::Texture movement_texture;
+    nc::AnimatedSprite movement;
+
+    nc::Texture jump_up_texture;
+    nc::AnimatedSprite jump_up;
+
+    nc::Texture jump_down_texture;
+    nc::AnimatedSprite jump_down;
 };
 
 } // namespace jmp
--- src/component/graphics.cpp
+++ src/component/graphics.cpp
@@ -1,5 +1,106 @@
 #include "component/graphics.h"
 
+#include <ncine/FileSystem.h>
+
+#include "component/transform.h"
+#include "input.h"
+
 namespace jmp
 {
+void SingleGraphicsComponent::update(const Input& input)
+{
+}
+
+CharacterGraphicsComponent::CharacterGraphicsComponent(TransformComponent& transform)
+    : idle_texture {PATH("img/hero/herochar_idle_anim_strip_4.png")}
+    , idle {&transform.node, &idle_texture}
+    , movement_texture {PATH("img/hero/herochar_run_anim_strip_6.png")}
+    , movement {nullptr, &movement_texture}
+    , jump_up_texture {PATH("img/hero/herochar_jump_up_anim_strip_3.png")}
+    , jump_up {nullptr, &jump_up_texture}
+    , jump_down_texture {PATH("img/hero/herochar_jump_down_anim_strip_3.png")}
+    , jump_down {nullptr, &jump_down_texture}
+{
+    // Idle animation
+    {
+        idle_texture.setMagFiltering(nc::Texture::Filtering::NEAREST);
+
+        auto anim = nc::RectAnimation(
+            0.125, nc::RectAnimation::LoopMode::ENABLED, nc::RectAnimation::RewindMode::FROM_START);
+
+        auto sprite_size = Vec2f(idle_texture.width() / 4.0f, idle_texture.height());
+
+        for (int i = 0; i < 4; ++i) {
+            anim.addRect(nc::Recti(i * sprite_size.x, 0.0f, sprite_size.x, sprite_size.y));
+        }
+
+        idle.addAnimation(MV(anim));
+        idle.setPaused(false);
+        idle.setLayer(1);
+    }
+
+    // Movement animation
+    {
+        movement_texture.setMagFiltering(nc::Texture::Filtering::NEAREST);
+
+        auto anim = nc::RectAnimation(
+            0.125, nc::RectAnimation::LoopMode::ENABLED, nc::RectAnimation::RewindMode::FROM_START);
+
+        auto sprite_size = Vec2f(movement_texture.width() / 6.0f, movement_texture.height());
+
+        for (int i = 0; i < 6; ++i) {
+            anim.addRect(nc::Recti(i * sprite_size.x, 0.0f, sprite_size.x, sprite_size.y));
+        }
+
+        movement.addAnimation(MV(anim));
+        movement.setPaused(false);
+        movement.setLayer(1);
+    }
+
+    // Jump animation
+    {
+        jump_up_texture.setMagFiltering(nc::Texture::Filtering::NEAREST);
+
+        auto jump_up_anim = nc::RectAnimation(
+            0.125, nc::RectAnimation::LoopMode::ENABLED, nc::RectAnimation::RewindMode::FROM_START);
+
+        auto sprite_size = Vec2f(jump_up_texture.width() / 3.0f, jump_up_texture.height());
+
+        for (int i = 0; i < 3; ++i) {
+            jump_up_anim.addRect(nc::Recti(i * sprite_size.x, 0.0f, sprite_size.x, sprite_size.y));
+        }
+
+        jump_up.addAnimation(MV(jump_up_anim));
+        jump_up.setPaused(false);
+        jump_up.setLayer(1);
+
+        jump_down_texture.setMagFiltering(nc::Texture::Filtering::NEAREST);
+
+        auto jump_down_anim = nc::RectAnimation(0.125f,
+            nc::RectAnimation::LoopMode::ENABLED,
+            nc::RectAnimation::RewindMode::FROM_START);
+
+        for (int i = 0; i < 3; ++i) {
+            jump_down_anim.addRect(nc::Recti(i * sprite_size.x, 0.0, sprite_size.x, sprite_size.y));
+        }
+
+        jump_down.addAnimation(MV(jump_down_anim));
+        jump_down.setPaused(false);
+        jump_down.setLayer(1);
+    }
+}
+
+void CharacterGraphicsComponent::update(const Input& input)
+{
+    // Set direction of the sprite
+    f32 movement_x = input.joystick.move.x;
+    if (movement_x) {
+        bool flipped_x = movement_x < 0.0;
+        movement.setFlippedX(flipped_x);
+        idle.setFlippedX(flipped_x);
+        jump_up.setFlippedX(flipped_x);
+        jump_down.setFlippedX(flipped_x);
+    }
+}
+
 } // namespace jmp
