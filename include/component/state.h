b3d96810093cdb9edf66349797a040a3a46292b0
Antonio Caggiano <info@antoniocaggiano.eu>
2021-01-02 19:19:13 +0100

    cmp: Character state component
    Refactor state related stuff moving it from Entity into its own
    component. Here we introduce a class hierarchy where the most basic
    state component has only a single state. At the same time we introduce a
    character state component with some internal states such as idle, move
    jump, and fall. Consider that this is closely coupled to the character
    graphics component.

--- include/component/state.h
+++ include/component/state.h
@@ -1,9 +1,53 @@
 #pragma once
 
+#include <nctl/String.h>
+
+#include "types.h"
+
 namespace jmp
 {
+class Input;
+class Entity;
+
 class StateComponent {
 public:
+    virtual void update(f32 dt, const Input& input, Entity& entity) = 0;
+};
+
+class SingleStateComponent : public StateComponent {
+public:
+    void update(f32 dt, const Input& input, Entity& entity) override;
+};
+
+class State
+{
+public:
+    enum Value { IDLE = 0, MOVE, JUMP_UP, JUMP_DOWN };
+
+    static State* get_state(State::Value state);
+
+    nctl::String name;
+    Value value;
+
+    virtual void enter(const Input& input, Entity& entity) = 0;
+    virtual void handle(const Input& input, Entity& entity) = 0;
+    virtual void update(const f32 dt, const Input& input, Entity& entity) = 0;
+    virtual void exit(Entity& entity) = 0;
+};
+
+const char* to_str(State& state);
+
+class CharacterStateComponent : public StateComponent {
+public:
+    static CharacterStateComponent& into(StateComponent& s);
+
+    CharacterStateComponent();
+
+    void update(f32 dt, const Input& input, Entity& entity) override;
+
+    void set_state(State::Value state, const Input& input, Entity& entity);
+
+    State* state;
 };
 
 } // namespace jmp
--- src/entity.cpp
+++ src/entity.cpp
@@ -9,215 +9,12 @@ namespace nc = ncine;
 
 namespace jmp
 {
-State* State::get_state(State::Value state)
-{
-    static UNIQUE<State> states[] = {
-        MK<IdleState>(),
-        MK<MoveState>(),
-        MK<JumpUpState>(),
-        MK<JumpDownState>(),
-    };
-
-    return states[state].get();
-}
-
-IdleState::IdleState()
-{
-    name = "IDLE";
-}
-
-#define CHAR_GFX(e) reinterpret_cast<CharacterGraphicsComponent&>(*e.graphics.value().get())
-
-void IdleState::enter(const Input& input, Entity& entity)
-{
-    entity.transform.node.addChildNode(&CHAR_GFX(entity).idle);
-}
-
-void can_fall(const Input& input, Entity& entity)
-{
-    // Can fall if there is no platform below
-    bool should_fall = true;
-
-    for (auto edge = entity.physics->body->GetContactList(); edge; edge = edge->next) {
-        auto normal = edge->contact->GetManifold()->localNormal;
-        if (edge->contact->GetFixtureA() == entity.physics->body->GetFixtureList()) {
-            normal = -normal;
-        }
-        if (normal.y > 0.5f) {
-            should_fall = false;
-            break;
-        }
-    }
-
-    if (should_fall) {
-        entity.set_state(input, State::JUMP_DOWN);
-    }
-}
-
-void IdleState::handle(const Input& input, Entity& entity)
-{
-    // Can move
-    if (input.joystick.move.x != 0.0) {
-        entity.set_state(input, State::MOVE);
-    }
-
-    // Can jump
-    if (input.joystick.a.just_down) {
-        entity.set_state(input, State::JUMP_UP);
-    }
-
-    can_fall(input, entity);
-}
-
-void IdleState::update(const f32 dt, const Input& input, Entity& entity)
-{
-    // Nothing
-}
-
-void IdleState::exit(Entity& entity)
-{
-    entity.transform.node.removeChildNode(&CHAR_GFX(entity).idle);
-}
-
-MoveState::MoveState()
-{
-    name = "MOVE";
-}
-
-void MoveState::enter(const Input& input, Entity& entity)
-{
-    entity.transform.node.addChildNode(&CHAR_GFX(entity).movement);
-}
-
-void MoveState::handle(const Input& input, Entity& entity)
-{
-    // Can go quiet
-    if (input.joystick.move.x == 0.0) {
-        entity.set_state(input, State::IDLE);
-    }
-
-    // Can jump
-    if (input.joystick.a.just_down) {
-        entity.set_state(input, State::JUMP_UP);
-    }
-
-    can_fall(input, entity);
-}
-
-void can_move_on_x(const Input& input, Entity& entity, f32 x_factor)
-{
-    f32 x_velocity = entity.physics->body->GetLinearVelocity().x;
-
-    bool opposite_move = (input.joystick.move.x < 0 && x_velocity >= 0) ||
-        (input.joystick.move.x >= 0 && x_velocity < 0);
-    bool within_limit =
-        fabs(entity.physics->body->GetLinearVelocity().x) < entity.physics->max_x_speed;
-
-    if (opposite_move || within_limit) {
-        auto force = b2Vec2(x_factor * input.joystick.move.x, 0.0f);
-        entity.physics->body->ApplyLinearImpulse(
-            force, entity.physics->body->GetWorldCenter(), true);
-    }
-}
-
-void MoveState::update(const f32 dt, const Input& input, Entity& entity)
-{
-    can_move_on_x(input, entity, entity.physics->velocity_factor);
-}
-
-void MoveState::exit(Entity& entity)
-{
-    entity.transform.node.removeChildNode(&CHAR_GFX(entity).movement);
-}
-
-JumpUpState::JumpUpState()
-{
-    name = "JUMP_UP";
-}
-
-void JumpUpState::handle(const Input& input, Entity& entity)
-{
-    // @todo Double jump?
-    if (entity.physics->body->GetLinearVelocity().y <= 0.0f) {
-        entity.set_state(input, State::JUMP_DOWN);
-    }
-}
-
-void JumpUpState::enter(const Input& input, Entity& entity)
-{
-    entity.transform.node.addChildNode(&CHAR_GFX(entity).jump_up);
-
-    auto force = b2Vec2(
-        entity.physics->jump_x_factor * input.joystick.move.x, entity.physics->jump_y_factor);
-    entity.physics->body->ApplyLinearImpulse(force, entity.physics->body->GetWorldCenter(), true);
-}
-
-void JumpUpState::update(const f32 dt, const Input& input, Entity& entity)
-{
-    // Can move a bit
-    can_move_on_x(input, entity, entity.physics->jump_x_factor);
-}
-
-void JumpUpState::exit(Entity& entity)
-{
-    entity.transform.node.removeChildNode(&CHAR_GFX(entity).jump_up);
-}
-
-JumpDownState::JumpDownState()
-{
-    name = "JUMP_DOWN";
-}
-
-void JumpDownState::enter(const Input& input, Entity& entity)
-{
-    entity.transform.node.addChildNode(&CHAR_GFX(entity).jump_down);
-}
-
-void JumpDownState::handle(const Input& input, Entity& entity)
-{
-    for (auto edge = entity.physics->body->GetContactList(); edge; edge = edge->next) {
-        auto& normal = edge->contact->GetManifold()->localNormal;
-        if (normal.y != 0.0) {
-            LOGI_X("Normal (%f, %f)", normal.x, normal.y);
-            entity.set_state(input, State::MOVE);
-            break;
-        }
-    }
-}
-
-void JumpDownState::update(const f32, const Input& input, Entity& entity)
-{
-    // Can move a bit
-    can_move_on_x(input, entity, entity.physics->jump_x_factor);
-}
-
-void JumpDownState::exit(Entity& entity)
-{
-    entity.transform.node.removeChildNode(&CHAR_GFX(entity).jump_down);
-}
-
-const char* to_str(State& state)
-{
-    return state.name.data();
-}
 
 Entity::Entity(nc::SceneNode& scene)
     : transform {scene}
     , graphics {MK<CharacterGraphicsComponent>(transform)}
+    , state {MK<CharacterStateComponent>()}
 {
-}
-
-void Entity::set_state(const Input& input, State::Value value)
-{
-    auto new_state = State::get_state(value);
-    if (state == new_state) {
-        return;
-    }
-
-    state->exit(*this);
-    new_state->enter(input, *this);
-
-    state = new_state;
 }
 
 void Entity::update(const f32 dt, const Input& input)
@@ -226,9 +23,10 @@ void Entity::update(const f32 dt, const Input& input)
         (*graphics)->update(input);
     }
 
-    state->handle(input, *this);
-    state->update(dt, input, *this);
-
+    if (state) {
+        (*state)->update(dt, input, *this);
+    }
+    
     physics->update();
 }
 
