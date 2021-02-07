4d4fb335e4b92d2399b6b93ac5e2d66be66623fc
Antonio Caggiano <info@antoniocaggiano.eu>
2021-02-07 16:54:49 +0100

    input: Escape key
    Add Escape key button to the input state object, and do not quit anymore
    when pressing it. We will close the application in a different way.

--- src/input.cpp
+++ src/input.cpp
@@ -4,9 +4,10 @@ namespace jmp
 {
 void Input::reset()
 {
-    this->mouse.left.just_down = false;
-    this->joystick.a.just_down = false;
-    this->joystick.x.just_down = false;
+    mouse.left.just_down = false;
+    joystick.a.just_down = false;
+    joystick.x.just_down = false;
+    key.esc.just_down = false;
 }
 
 } // namespace jmp
